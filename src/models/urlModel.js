73aec77e343ebe388477fde16a17494f811428ad
Joy bhattacharya <joy.arya7381@gmail.com>
2022-01-23 23:34:04 +0530

    URL Shortner

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const urlSchema = new mongoose.Schema({
 
     urlCode: { type: String, unique: true, lowercase: true, trim: true },
-    longUrl: { type: String, required: "Long URL is required", }, // should be a valid url
+    longUrl: { type: String, required: "Long URL is required",trim:true }, // should be a valid url
     shortUrl: { type: String, required: true, unique: true }
 
 }, { timestamps: true })
 
-module.exports = mongoose.model('URL_Collection', urlSchema)
+module.exports = mongoose.model('project3_urls', urlSchema)
