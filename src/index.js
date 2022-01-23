f6acc489146f80139cc8b22fcb292140879bff7a
Joy bhattacharya <92808579+Joy-300799@users.noreply.github.com>
2022-01-23 22:42:22 +0530

    updated Connection String

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/Immortals-group7?retryWrites=true&w=majority", { useNewUrlParser: true })
-    .then(() => console.log('Immortals are ready to roll !'))
+mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/Joy_Bhattacharya-DB?authSource=admin&replicaSet=atlas-wwe75z-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", { useNewUrlParser: true })
+    .then(() => console.log('DB connected.'))
     .catch(err => console.log(err))
 
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(3000, function() {
+    console.log('Express app running on port ' + 3000)
 });
